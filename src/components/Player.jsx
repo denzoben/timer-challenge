a27c16fa575e59d56afeb89968962e29ee5dd63b
Sudipra Biswas <sudipra.biswas@cytel.com>
2024-08-07 17:27:40 +0530

    Handling player name change using two-way binding and state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  const [showPlayerName, setShowPlayerName] = useState(false);
+
+  function handlePlayerNameChange(event) {
+    setShowPlayerName(false);
+    setPlayerName(event.target.value);
+  }
+
+  function handlePlayerName() {
+    setShowPlayerName(true);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity!</h2>
+      <h2>Welcome {showPlayerName ? playerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          type="text"
+          onChange={handlePlayerNameChange}
+          value={playerName}
+        />
+        <button onClick={handlePlayerName}>Set Name</button>
       </p>
     </section>
   );
